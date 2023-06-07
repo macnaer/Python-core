828699baf3be1fac8d99cc504052aeb7a9e301c5
macnaer <trofimchuk.an@gmail.com>
2023-06-07 10:48:34 +0300

    Init

---
+++ 01. Intro/start.py
@@ -0,0 +1,16 @@
+# print("Hello world!")
+'''a = 10
+print(type(a), a)
+a = 10.8
+print(type(a), a)
+a = True
+print(type(a), a)
+a = "Apple"
+print(type(a), a)
+print(len(a))'''
+
+text = "Hello my friend!\n How are you today?"
+print(text)
+print("Len: ", len(text))
+print(text.find("?"))
+print(text.islower())
