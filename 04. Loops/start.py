440c20c20072205336b1b39324e34d2800986f61
macnaer <trofimchuk.an@gmail.com>
2023-06-19 09:59:31 +0300

    Typle ams sets

--- 04. Loops/start.py
+++ 04. Loops/start.py
@@ -1,9 +1,9 @@
-people = ["John", "Adam", "Sara", "Paul", "Eva"]
+# people = ["John", "Adam", "Sara", "Paul", "Eva"]
 
-print(f"Index 'Sara' = {people.index('Sara')}")
+# print(f"Index 'Sara' = {people.index('Sara')}")
 
-for person in people:
-    print(person)
+# for person in people:
+#     print(person)
 
 # for item in people:
 #     if item == "Sara":
@@ -16,3 +16,20 @@ for person in people:
 # while count <= 10:
 #     print(f"Count: {count}")
 #     count += 1
+
+# number = int(input("Enter number: "))
+
+# print(number, type(number))
+
+exit = False
+# while not exit:
+while exit != True:
+    choise = int(input(f"Buy 36.57\tSale 37.45\n1. Buy\n2. Sale\n0. Exit "))
+    if choise == 1:
+        print("Buy")
+    elif choise == 2:
+        print("sale")
+    elif choise == 0:
+        exit = True
+    else:
+        print("Wrong choise")
---
+++ 05. Typles/start.py
@@ -0,0 +1,29 @@
+lang = ('Python', 'C#', "PHP", "Java", "TypeScript", "C#")
+
+# print(len(lang))
+# print(type(lang))
+# print(lang)
+# lang[5] = "C++"
+# print(lang[5])
+
+# fruits = ("Apple", "Banana", "Orange", "Cherry")
+# print(f"Type: {type(fruits)}\nLength: {len(fruits)}\nData: {fruits}")
+# fruits[0] = "Banana"  # Error
+
+# print('Banana' in fruits)
+# del fruits
+
+fruits = {"Apple", "Banana", "Orange", "Cherry"}
+print(f"Type: {type(fruits)}\nLength: {len(fruits)}\nData: {fruits}")
+print('Banana' in fruits)
+fruits.add("Lemon")
+print(f"Type: {type(fruits)}\nLength: {len(fruits)}\nData: {fruits}")
+print("---------------------Lemon===================")
+fruits.add("Lemon")
+fruits.add("Lemon")
+fruits.add("Apple")
+fruits.add("Lemon")
+print(f"Type: {type(fruits)}\nLength: {len(fruits)}\nData: {fruits}")
+fruits.remove("Orange")
+print(f"Type: {type(fruits)}\nLength: {len(fruits)}\nData: {fruits}")
+del fruits
